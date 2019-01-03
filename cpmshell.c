81df7fc691858da6d368f1848228453c7fa02e55
Carlos Rodriguez <carlosrg1997@hotmail.com>
2019-01-03 17:25:59 +0100

    added readConf

--- cpmshell.c
+++ cpmshell.c
@@ -6,8 +6,13 @@
 #include "lib/dir.h"
 #include "lib/type.h"
 
+char c1 = ' ';
+char c2 = ' ';
+
+void readConf();
 
 int main(){
+    readConf();
 	char *actualDir; 
 	actualDir = "A";
 	char query[255];
@@ -29,9 +34,6 @@ int main(){
 		}else if(strcmp(query,"A:") == 0){
 			actualDir = "A";
 		}else if(strcmp(query,"HELP") == 0){
-            char command[100];
-            // cat lib/help.txt
-            //sprintf(command, "%s", use_dir(query,actualDir));
 			system("cat lib/help.txt");
         }
         else{
@@ -43,6 +45,36 @@ int main(){
 		strtok(fgets(query,255,stdin),"\n");
 	}
 	return 0;
-	
+}
+
+void readConf(){
+    FILE * fp;
+    fp = fopen("./.cmpconfig.txt", "r");
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    
+    if (fp == NULL){
+        printf("ERROR");
+        exit(EXIT_FAILURE);
+    }
+    int i = 0;
+    while ((read = getline(&line, &len, fp)) != -1) {
+        printf("Retrieved line of length %zu:\n", read);
+        printf("%s", line);
+        printf("%c", line[0]);
+        if(i = 0){
+            c1 = line[0];
+            i++;
+        }else{
+            c2 = line[0];
+        }
+    }
+    printf("\n");
+    fclose(fp);
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
+    
 }
 
