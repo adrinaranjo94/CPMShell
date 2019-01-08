eebc28409fe0a05d5fc01de7d894f236b4a83446
AdriNaranjo94 <adrian.naranjo@live.u-tad.com>
2019-01-08 18:03:53 +0100

    config directories function implemented

--- cpmshell.c
+++ cpmshell.c
@@ -5,18 +5,23 @@
 
 #include "lib/dir.h"
 #include "lib/type.h"
+#include "lib/era.h"
 
 char c1 = ' ';
 char c2 = ' ';
+char auxDir[3];
 
 void readConf();
 
+char* getDir(char dir);
+
+
 int main(){
     readConf();
-printf("C1 %c",c1);
-printf("C2 %c",c2);
-	char *actualDir; 
-	actualDir = "A";
+//printf("C1 %c",c1);
+//printf("C2 %c",c2);
+	char actualDir[2]; 
+	sprintf(actualDir, "%c", c1);
 	char query[255];
 	printf("%s:> ",actualDir);
 	
@@ -30,22 +35,33 @@ printf("C2 %c",c2);
 			char command[100];
 			sprintf(command, "%s", use_type(query,actualDir));
 			system(command);
-		}else if(strcmp(query,"A:") == 0){
-			actualDir = "B";
-		}else if(strcmp(query,"B:") == 0){
-			actualDir = "A";
+		}else if(strstr(query,"ERA") != NULL){
+			char command[100];
+			sprintf(command, "%s", use_era(query,actualDir));
+			system(command);
+		}else if(strcmp(query,getDir(c1)) == 0){
+			sprintf(actualDir, "%c", c1);
+		}else if(strcmp(query,getDir(c2)) == 0){
+			sprintf(actualDir, "%c", c2);
 		}else if(strcmp(query,"HELP") == 0){
 			system("cat lib/help.txt");
         }
         else{
-			printf("no se\n");
-			printf("%s",query);
+			printf("comando desconocido\n");
+			//printf("%s",query);
 		}
 		printf("%s:> ",actualDir);
 		
 		strtok(fgets(query,255,stdin),"\n");
 	}
 	return 0;
+}
+
+char* getDir(char dir){
+	sprintf(auxDir,"%c:",dir);
+	
+	return auxDir;
+	
 }
 
 void readConf(){
@@ -63,6 +79,7 @@ void readConf(){
     while ((read = getline(&line, &len, fp)) != -1) {
         if(i == 0){
             c1 = line[0];
+            
             i++;
         }else{
             c2 = line[0];
---
+++ lib/era.h
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdlib.h>
+
+void substringEra(char s[], char sub[], int p, int l);
+
+char* selectDirEra(char dir[]);
+
+char* checkParametersEra(char path[],char actualDir[]);
+
+char* methodEra(char dir[]);
+
+char* use_era(char query[],char actualDir[]);
